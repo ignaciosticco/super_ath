5544bf2d0bdd9e50b698f4f086d33dce6c408baf
ignaciosticco <ignaciosticco@gmail.com>
2017-03-20 17:47:49 -0300

    mediciones de narrow

---
+++ narrow/medio/analisis_narrow2.py
@@ -0,0 +1,127 @@
+import matplotlib.pyplot as plt
+import pylab
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from pylab import arange,pi,sin,cos,sqrt,savefig
+
+golden_mean = (math.sqrt(5)-1.0)/2.0        # Aesthetic ratio
+fig_width = 3+3/8                           # width  in inches
+fig_height = fig_width*golden_mean          # height in inches
+fig_size =  [fig_width,fig_height]
+
+params = {'backend': 'ps',
+          'axes.titlesize': 8,
+          'axes.labelsize': 9,
+          'axes.linewidth': 0.5, 
+          'axes.grid': True,
+          'axes.labelweight': 'normal',  
+          'font.family': 'serif',
+          'font.size': 8.0,
+          'font.weight': 'normal',
+          'text.color': 'black',
+          'xtick.labelsize': 8,
+          'ytick.labelsize': 8,
+          'text.usetex': True,
+          'legend.fontsize': 8,
+          'figure.dpi': 300,
+          'figure.figsize': fig_size,
+          'savefig.dpi': 300,
+         }
+
+pylab.rcParams.update(params)
+
+# medio= alude a el punto (x=10,y=10)
+t_medio = []
+x_medio = []
+vx_medio = [] 
+f_granular_medio = []
+f_social_medio = []
+f_desered_medio = []
+f_desired_medio=[]
+f_neta=[]
+comp=[]
+
+m=70
+tau=0.5
+
+vd = np.linspace(1,17,17)
+
+for j in vd:
+    v=str(int(vd[int(j)-1]))
+    data = np.genfromtxt("in_narrow_print_vd%s.txt" %v, delimiter = ' ')
+    v=int(v)
+
+    t = data[:,0]
+    x = data[:,1]
+    y = data[:,2]
+    vx = data[:,3]
+    vy = data[:,4]
+    f_granular = data[:,5]
+    f_social = data[:,6]
+    fx = data[:,7]
+    fy = data[:,8]
+    campo_externo=2000.0*vd[int(j)-1]
+
+    # central= particula que se mueve 
+    x_central_dmedio=[]   # distancia al centro (10,10) del peaton que se mueve (central) 
+
+    x_central=[]
+    vx_central=[]
+    t_cental=[]
+    f_granular_central=[]
+    f_social_central=[]
+    f_neta_central=[]
+
+
+
+    ## filtrar la particula central ##
+    i=0
+    suma_vel=0
+    while i<len(vx)/3:
+        x_central_dmedio+=[abs(x[i*3]-10)]
+        x_central+=[x[i*3]]
+        vx_central+= [vx[i*3]]
+        t_cental+= [t[i*3]]
+        f_granular_central+=[f_granular[i*3]]
+        f_social_central+=[f_social[i*3]]
+        f_neta_central+=[((v-vx[i*3])*m/tau)+f_social[i*3]+campo_externo-f_granular[i*3]] # F neta part central(t)
+        i+=1
+    index_min = np.argmin(x_central_dmedio)
+
+    ## Valores de las variables evaluados en la posicion central x=10 y=10
+    t_medio+=[t_cental[index_min]]
+    x_medio+=[x_central[index_min]]
+    vx_medio+=[vx_central[index_min]] 
+    f_granular_medio+=[f_granular_central[index_min]]
+    f_social_medio+=[f_social_central[index_min]]
+    f_desired_medio+=[(v-vx_central[index_min])*m/tau]
+    f_neta+=[f_neta_central[index_min]]
+    comp+=[0.08*math.log(2000.0*v/2000)]
+
+
+
+#plt.plot(vd,f_desired_medio,'r',label='desired',lw=0.7,zorder=2)
+plt.semilogy(vd,f_granular_medio,'b',label='roz',lw=0.7,zorder=2) 
+plt.semilogy(vd,vx_medio,'r',label='speed',lw=0.7,zorder=1)
+plt.semilogy(vd,comp,'g',label='comp',lw=0.7,zorder=3)
+
+#plt.plot(vd,f_neta,'o',zorder=2)
+
+#plt.plot(vd,v_min_inv,'b',label='speed',lw=0.7,zorder=2) 
+#plt.plot(t_cental,x_central,'b',label='speed',lw=0.7,zorder=2)
+#plt.plot(time,f_roz,'r',label='friction',lw=0.7,zorder=2) 
+#plt.plot(time,v_speed_100,'g',label='speed',lw=0.7,zorder=2) 
+#plt.plot(1,1,'w.',zorder=3) 
+#pylab.xticks(np.arange(0,8,2))
+#pylab.yticks(np.arange(20,100,20))
+pylab.xlabel('v$_d$~(m/s)')
+pylab.ylabel('value')
+#pylab.legend()
+#pylab.xlim(2, 18)
+#pylab.ylim(0, 0.5)
+#lgd=plt.legend() 
+#lgd.set_visible(True) 
+plt.legend(loc='mid right',labelspacing=0.2,borderpad=0.1,handletextpad=0.1)
+pylab.savefig('rozz.eps', format='eps', dpi=300, bbox_inches='tight')
+
